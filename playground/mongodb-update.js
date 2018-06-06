76717a57ecb9a1164fe882bc74f86061aefaf815
Mark Tice <marktice@protonmail.com>
2018-06-06 14:29:22 +1000

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b175ac40ebac14d3070ac54')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // })
+  
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b17423d9fcb984d4d039008')
+  }, {    
+      $set: {
+        name: 'Mark'
+      },
+      $inc: {
+        age: 1
+      }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+
+
+  // db.close();
+});
