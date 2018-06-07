4680e0d156769fcb3ba0acda0fd3b55a541e835d
Mark Tice <marktice@protonmail.com>
2018-06-07 21:40:35 +1000

    Add POST /todos route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,21 @@ const { User } = require('./models/user');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save()
+    .then((doc) => {
+      res.send(doc);
+    }).catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
