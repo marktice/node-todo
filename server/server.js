0732db0ec5aa7f03077f3b466ff7586f210f3fce
Mark Tice <marktice@protonmail.com>
2018-06-08 16:22:02 +1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
       res.send({ todos });
     }).catch((err) => {
       res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params; // req.params is an object, w/key we gave(:id) { "id": "123" }
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch((err) => {
+      res.status(400).send();
     });
 });
 
