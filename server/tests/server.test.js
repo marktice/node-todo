6a9a0e3a366e730a2db1f99577b44e5d9bb86574
Mark Tice <marktice@protonmail.com>
2018-06-08 13:38:51 +1000

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
-// make sure db is empty
+// make sure db is empty before every test
 beforeEach((done) => {
   Todo.remove({})
     .then(() => done());
