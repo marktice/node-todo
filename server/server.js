a3828373df188db82b3bbd6d0f40488f2f530fda
Mark Tice <marktice@protonmail.com>
2018-06-27 22:04:26 +1000

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -118,7 +118,6 @@ app.post('/users', (req, res) => {
       return user.generateAuthToken();
     })
     .then((token) => {
-      // when you prefact a header with: 'x-' is a custom header
       res.header('x-auth', token).send(user);
     })
     .catch((err) => {
@@ -128,6 +127,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch((err) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
